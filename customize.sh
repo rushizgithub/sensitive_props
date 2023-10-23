d5e81eb39fd2aa2d12dc778d0350fc9d09214fb5
Rushi Ranpise <136841749+rushizgithub@users.noreply.github.com>
2023-10-23 23:19:10 +0530

    Add files via upload

--- customize.sh
+++ customize.sh
@@ -1,5 +1,15 @@
-$BOOTMODE || abort "! Recovery is not supported"
+#!/system/bin/sh
 
-ui_print "- Reset sensitive props"
+if $BOOTMODE; then
+    ui_print "- Installing from Magisk app"
+else
+    ui_print "*********************************************************"
+    ui_print "! Install from recovery is NOT supported"
+    ui_print "! Recovery sucks"
+    ui_print "! Please install from Magisk app"
+    abort    "*********************************************************"
+fi
+
+chmod 755 "$MODPATH/service.sh"
 
 sh "$MODPATH/service.sh" 2>&1
--- service.sh
+++ service.sh
@@ -1,3 +1,5 @@
+#!/system/bin/sh
+
 MAGISKTMP="$(magisk --path)" || MAGISKTMP=/sbin
 MODPATH="${0%/*}"
 
@@ -7,17 +9,57 @@ MODPATH="${0%/*}"
 
 . "$MODPATH/resetprop.sh"
 
-if [ "$(cat /sys/fs/selinux/enforce)" != "1" ]; then
-    chmod 660 /sys/fs/selinux/enforce
+# Hiding SELinux | Permissive status
+selinux="$(resetprop ro.build.selinux)"
+[ -z "$selinux" ] || resetprop --delete ro.build.selinux
+
+# Hiding SELinux | Use toybox to protect *stat* access time reading
+if [[ "$(toybox cat /sys/fs/selinux/enforce)" == "0" ]]; then
+    chmod 640 /sys/fs/selinux/enforce
     chmod 440 /sys/fs/selinux/policy
 fi
+
+# Magisk recovery mode
+maybe_resetprop ro.bootmode recovery unknown
+maybe_resetprop ro.boot.mode recovery unknown
+maybe_resetprop vendor.bootmode recovery unknown
+maybe_resetprop vendor.boot.mode recovery unknown
+
+# Remove LineageOS props
+# This should only confuse the OTA updater
+resetprop --delete ro.lineage.build.version
+resetprop --delete ro.lineage.build.version.plat.rev
+resetprop --delete ro.lineage.build.version.plat.sdk
+resetprop --delete ro.lineage.device
+resetprop --delete ro.lineage.display.version
+resetprop --delete ro.lineage.releasetype
+resetprop --delete ro.lineage.version
+resetprop --delete ro.lineagelegal.url
+
+# Create a personalized system.prop
+getprop | grep "userdebug" >> "$MODPATH/tmp.prop"
+getprop | grep "test-keys" >> "$MODPATH/tmp.prop"
+getprop | grep "lineage_"  >> "$MODPATH/tmp.prop"
+
+sed -i 's/\[//g'  "$MODPATH/tmp.prop"
+sed -i 's/\]//g'  "$MODPATH/tmp.prop"
+sed -i 's/: /=/g' "$MODPATH/tmp.prop"
+
+sed -i 's/userdebug/user/g' "$MODPATH/tmp.prop"
+sed -i 's/test-keys/release-keys/g' "$MODPATH/tmp.prop"
+sed -i 's/lineage_//g' "$MODPATH/tmp.prop"
+
+sort -u "$MODPATH/tmp.prop" > "$MODPATH/system.prop"
+rm "$MODPATH/tmp.prop"
 
 while [ "$(getprop sys.boot_completed)" != 1 ]; do
     sleep 1
 done
 
-# these props should be set after boot completed to avoid breaking some device features
+# Make sure we set those props
+resetprop -n --file "$MODPATH/system.prop"
 
+# these props should be set after boot completed to avoid breaking some device features
 check_resetprop ro.boot.vbmeta.device_state locked
 check_resetprop ro.boot.verifiedbootstate green
 check_resetprop ro.boot.flash.locked 1
@@ -35,14 +77,8 @@ check_resetprop vendor.boot.verifiedbootstate green
 check_resetprop sys.oem_unlock_allowed 0
 check_resetprop init.svc.flash_recovery stopped
 
-maybe_resetprop ro.bootmode recovery unknown
-maybe_resetprop ro.boot.mode recovery unknown
-maybe_resetprop vendor.bootmode recovery unknown
-maybe_resetprop vendor.boot.mode recovery unknown
 maybe_resetprop ro.boot.hwc CN GLOBAL
 maybe_resetprop ro.boot.hwcountry China GLOBAL
-selinux="$(resetprop ro.build.selinux)"
-[ -z "$selinux" ] || resetprop --delete ro.build.selinux
 
 for prefix in system vendor system_ext product oem odm vendor_dlkm odm_dlkm; do
     check_resetprop ro.${prefix}.build.type user
